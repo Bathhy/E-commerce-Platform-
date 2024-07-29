37459d54803d91d287bbe404976d4456a70b85b8
keosambath <keosambathnov@gmail.com>
2024-07-29 08:59:40 +0700

    blah

--- src/constant/Constant.java
+++ src/constant/Constant.java
@@ -4,4 +4,9 @@ public class Constant {
     public static String imgurl = "https://i.pinimg.com/736x/eb/de/de/ebdede6f0b683e1dfb1a23a3a4fdb3f8.jpg";
     public static String iconpath = "src/Resources/images/usericon.png";
     public static String fakeimage = "src/Resources/images/5e1cbfb2458fa49e22a783a5731793bd.jpg";
+    public static String userdbname = "root";
+    public static String userdbpass = "Leonard1234##";
+    public static String dburl = "jdbc:mysql://127.0.0.1:3306/testdbjava";
+    public static int screenwidth = 800;
+    public static int screenheight = 600;
 }
